e14c7caffb67f243a5750c7ed9756ac743377ed5
Olivier Lecarme <olecarme@resonanceicp.fr>
2024-06-14 11:10:52 +0200

    Added "DataSet" for raw XY data handling

---
+++ RICPFitter/Data/DataSet.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RICPFitter.Data
+{
+    /// <summary>
+    /// Set of XY data
+    /// </summary>
+    public class DataSet : IDataSet
+    {
+        private double[] xData;
+        private double[] yData;
+
+        /// <inheritdoc/>
+        public string Title { get; set; }
+
+        /// <inheritdoc/>
+        public string XLabel { get; set; }
+
+        /// <inheritdoc/>
+        public string YLabel { get; set; }
+
+        /// <inheritdoc/>
+        public double[] XData
+        {
+            get => xData;
+            set
+            {
+                if (xData != value)
+                {
+                    xData = value;
+                    if (yData != null) InputDataChanged?.Invoke(this);
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public double[] YData
+        {
+            get => yData;
+            set
+            {
+                if (yData != value)
+                {
+                    yData = value;
+                    if (xData != null) InputDataChanged?.Invoke(this);
+                }
+            }
+        }
+
+        /// <summary>
+        /// New empty dataset
+        /// </summary>
+        public DataSet()
+        {
+
+        }
+
+        /// <summary>
+        /// New dataset with input data
+        /// </summary>
+        /// <param name="xData"></param>
+        /// <param name="yData"></param>
+        /// <param name="title"></param>
+        /// <param name="xLabel"></param>
+        /// <param name="yLabel"></param>
+        public DataSet(double[] xData, double[] yData, string title = null, string xLabel = null, string yLabel = null) : this()
+        {
+            this.xData = xData;
+            this.yData = yData;
+            Title = title;
+            XLabel = xLabel;
+            YLabel = yLabel;
+            InputDataChanged?.Invoke(this);
+        }
+
+        /// <inheritdoc/>
+        public event Action<DataSet> InputDataChanged;
+
+    }
+}
---
+++ RICPFitter/Data/IDataSet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RICPFitter.Data
+{
+    /// <summary>
+    /// Interface describing a set of XY data
+    /// </summary>
+    public interface IDataSet
+    {
+        /// <summary>
+        /// Title
+        /// </summary>
+        string Title { get; set; }
+
+        /// <summary>
+        /// X data
+        /// </summary>
+        double[] XData { get; set; }
+
+        /// <summary>
+        /// X label or description
+        /// </summary>
+        string XLabel { get; set; }
+
+        /// <summary>
+        /// Y data
+        /// </summary>
+        double[] YData { get; set; }
+
+        /// <summary>
+        /// Y label or description
+        /// </summary>
+        string YLabel { get; set; }
+
+        /// <summary>
+        /// Fired when X or Y data change
+        /// </summary>
+        event Action<DataSet> InputDataChanged;
+    }
+}

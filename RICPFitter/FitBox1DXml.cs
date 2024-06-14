ba9c9e57113f9de15cb4af35c6128e03b4ad5d43
Olivier Lecarme <olecarme@resonanceicp.fr>
2024-06-14 11:13:29 +0200

    Added "IFitBox1D" as all-in-one toolbox for easy integration

---
+++ RICPFitter/FitBox1DXml.cs
@@ -0,0 +1,100 @@
+﻿using RICPFitter.Collections;
+using RICPFitter.Data;
+using RICPFitter.Functions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RICPFitter
+{
+    /// <summary>
+    /// Class designed to fit any 1D curve on input data
+    /// </summary>
+    public class FitBox1DXml : IFitBox1D
+    {
+        private IFittable fitFunction;
+        private IDataSet inputData;
+
+        /// <inheritdoc/>
+        public IDataSet InputData
+        {
+            get => inputData;
+            set
+            {
+                if (inputData != value)
+                {
+                    if (inputData != null) inputData.InputDataChanged -= InputData_InputDataChanged;
+                    inputData = value;
+                    if (inputData != null)
+                    {
+                        InputData_InputDataChanged(inputData);
+                        inputData.InputDataChanged += InputData_InputDataChanged;
+                    }
+                }
+            }
+        }
+
+        private void InputData_InputDataChanged(IDataSet data)
+        {
+            InputDataChanged?.Invoke(data.XData, data.YData);
+        }
+
+        /// <inheritdoc/>
+        public IFittable FitFunction
+        {
+            get => fitFunction;
+            set
+            {
+                if (fitFunction != value)
+                {
+                    if (fitFunction != null) fitFunction.FitPerformed -= FitFunction_FitPerformed;
+                    fitFunction = value;
+                    if (fitFunction != null)
+                    {
+                        FitFunctionChanged?.Invoke(fitFunction);
+                        fitFunction.FitPerformed += FitFunction_FitPerformed;
+                    }
+                }
+            }
+        }
+
+        private void FitFunction_FitPerformed(IFittable func, double[] x, double[] y)
+        {
+            FitDataChanged?.Invoke(func, x, y);
+        }
+
+        /// <inheritdoc/>
+        public IFunctionCollection ListOfFitFunctions { get; set; }
+
+        /// <summary>
+        /// New empty fit box 1D
+        /// </summary>
+        public FitBox1DXml()
+        {
+
+        }
+
+        /// <summary>
+        /// New fit box 1D with fit functions definitions
+        /// </summary>
+        /// <param name="xmlFilePath">XML file path containing the definitions of the fit functions</param>
+        public FitBox1DXml(string xmlFilePath)
+        {
+            ListOfFitFunctions = new FunctionCollectionXml(xmlFilePath);
+            if (ListOfFitFunctions != null)
+            {
+                FitFunction = ListOfFitFunctions.Functions[0];
+            }
+        }
+
+        /// <inheritdoc/>
+        public event Action<double[], double[]> InputDataChanged;
+        /// <inheritdoc/>
+        public event Action<IFittable, double[], double[]> FitDataChanged;
+        /// <inheritdoc/>
+        public event Action<IFittable> FitFunctionChanged;
+    }
+}
---
+++ RICPFitter/IFitBox1D.cs
@@ -0,0 +1,40 @@
+﻿using RICPFitter.Collections;
+using RICPFitter.Data;
+using RICPFitter.Functions;
+using System;
+
+namespace RICPFitter
+{
+    public interface IFitBox1D
+    {
+        /// <summary>
+        /// Fit function
+        /// </summary>
+        IFittable FitFunction { get; set; }
+
+        /// <summary>
+        /// Input data
+        /// </summary>
+        IDataSet InputData { get; set; }
+
+        /// <summary>
+        /// List of available fit functions
+        /// </summary>
+        IFunctionCollection ListOfFitFunctions { get; set; }
+
+        /// <summary>
+        /// Fired when fit data are changed
+        /// </summary>
+        event Action<IFittable, double[], double[]> FitDataChanged;
+
+        /// <summary>
+        /// Fired when the fit function type is changed
+        /// </summary>
+        event Action<IFittable> FitFunctionChanged;
+
+        /// <summary>
+        /// Fired when input data are changed
+        /// </summary>
+        event Action<double[], double[]> InputDataChanged;
+    }
+}
